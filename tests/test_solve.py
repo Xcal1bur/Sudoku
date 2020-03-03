b56c11f8075fcfdae8a641ffc09ab20746981d02
Xcal1bur <david.voigt1998@gmail.com>
2020-03-03 19:56:21 +0100

    Initial commit

--- tests/test_solve.py
+++ tests/test_solve.py
@@ -1,1 +1,19 @@
+import numpy as np
 from src import solve
+from src import generate
+
+grid1: np.matrix = np.matrix([
+    [0, 9, 2, 0, 0, 0, 8, 7, 0],
+    [6, 0, 0, 4, 0, 7, 0, 0, 2],
+    [3, 0, 0, 5, 0, 2, 0, 0, 9],
+    [0, 7, 1, 0, 0, 0, 5, 2, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 4, 6, 0, 0, 0, 9, 8, 0],
+    [7, 0, 0, 1, 0, 5, 0, 0, 4],
+    [2, 0, 0, 8, 0, 3, 0, 0, 7],
+    [0, 5, 4, 0, 0, 0, 2, 3, 0]
+    ])
+
+def test_backtrack_solve():
+    assert type(solve.backtrack_solve(grid1)) == np.matrix
+    assert generate.validate(solve.backtrack_solve(grid1)) == True
