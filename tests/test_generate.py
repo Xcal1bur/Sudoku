6e646b50b306a8111b05e0c9f01eb27460f93f08
Xcal1bur <david.voigt1998@gmail.com>
2020-03-12 21:55:34 +0100

    Minor changes since some function have been moved to solve module.

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -15,38 +15,13 @@ grid1: np.matrix = np.matrix([
     [0, 5, 4, 0, 0, 0, 2, 3, 0]
     ])
 
-def test_possible():
-    assert type(generate.possible(0, 0, 1, grid1)) == bool
-    assert generate.possible(0, 0, 1, grid1) == True
-    assert generate.possible(0, 0, 4, grid1) == True
-    assert generate.possible(1, 5, grid1[1, 5], grid1) == True
-    assert generate.possible(6, 5, grid1[6, 5], grid1) == True
-    # Test subgrid detection
-    assert generate.possible(0, 0, 9, grid1) == False
-    # Test row detection
-    assert generate.possible(0, 0, 2, grid1) == False
-    # Test column detection
-    assert generate.possible(0, 0, 7, grid1) == False
-    # Test all possible numbers for y=0, x=0
-    pos: dict = {}
-    for x in range(1, 10):
-        pos[x] = generate.possible(0, 0, x, grid1)
-    assert pos == {1: True, 2: False, 3: False, 4: True, 5: True, 6: False, 7: False, 8: False, 9: False}
-
-def test_validate():
-    assert type(generate.validate(grid1)) == bool
-    assert generate.validate(grid1) == True
-    assert generate.validate(solve.backtrack_solve(grid1)) == True
-
 def test_generate_sudoku():
-    assert type(generate.generate_sudoku()) == np.matrix
-    assert generate.validate(generate.generate_sudoku()) == True
+    assert type(generate.generate_full_sudoku()) == np.matrix
+    assert solve.validate(generate.generate_full_sudoku()) == True
     # Check if no zero occur / the sudoku has been filled completly.
-    grid = generate.generate_sudoku().tolist()
+    grid = generate.generate_full_sudoku().tolist()
     zero = False
     for i in range(len(grid)):
         if 0 in grid[i]:
             zero = True
     assert zero == False
-
-
--- tests/test_solve.py
+++ tests/test_solve.py
@@ -14,6 +14,28 @@ grid1: np.matrix = np.matrix([
     [0, 5, 4, 0, 0, 0, 2, 3, 0]
     ])
 
-def test_backtrack_solve():
-    assert type(solve.backtrack_solve(grid1)) == np.matrix
-    assert generate.validate(solve.backtrack_solve(grid1)) == True
+def test_possible():
+    assert type(solve.possible(0, 0, 1, grid1)) == bool
+    assert solve.possible(0, 0, 1, grid1) == True
+    assert solve.possible(0, 0, 4, grid1) == True
+    assert solve.possible(1, 5, grid1[1, 5], grid1) == True
+    assert solve.possible(6, 5, grid1[6, 5], grid1) == True
+    # Test subgrid detection
+    assert solve.possible(0, 0, 9, grid1) == False
+    # Test row detection
+    assert solve.possible(0, 0, 2, grid1) == False
+    # Test column detection
+    assert solve.possible(0, 0, 7, grid1) == False
+    # Test all possible numbers for y=0, x=0
+    pos: dict = {}
+    for x in range(1, 10):
+        pos[x] = solve.possible(0, 0, x, grid1)
+    assert pos == {1: True, 2: False, 3: False, 4: True, 5: True, 6: False, 7: False, 8: False, 9: False}
+
+def test_validate():
+    assert type(solve.validate(grid1)) == bool
+    assert solve.validate(grid1) == True
+    assert solve.validate(solve.solve(grid1, [])) == True
+
+def test_solve():
+    assert type(solve.solve(grid1, [])) == list
